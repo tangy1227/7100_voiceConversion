c8345351ff97d85807df30d62eb6e5a3d8ebfc0c
tangy1227 <owentang1227@gmail.com>
2024-01-23 15:36:17 -0500

    semester start update

--- vocoder-HifiGAN/hifiGAN_inference_e2e.py
+++ vocoder-HifiGAN/hifiGAN_inference_e2e.py
@@ -72,7 +72,7 @@ def inference(a):
             audio = audio * MAX_WAV_VALUE
             audio = audio.cpu().numpy().astype('int16')
 
-            output_file = os.path.join(a.output_dir, os.path.splitext(name)[0] + '_generated_e2e.wav')
+            output_file = os.path.join(a.output_dir, os.path.splitext(name)[0] + '.wav') # output file name
             write(output_file, h.sampling_rate, audio)
             print(output_file)
 
